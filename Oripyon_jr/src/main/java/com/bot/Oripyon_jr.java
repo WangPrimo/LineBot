82009f940bf095642ae0d3b7f61a21607ccc195a
WangPrimo <primo_wang@yahoo.com.tw>
2017-03-23 14:01:26 +0800

    Update Oripyon_jr.java
    LineMessagingClientImpl

--- Oripyon_jr/src/main/java/com/bot/Oripyon_jr.java
+++ Oripyon_jr/src/main/java/com/bot/Oripyon_jr.java
@@ -28,6 +28,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linecorp.bot.client.LineMessagingService;
+import com.linecorp.bot.client.LineMessagingClientImpl;
 import com.linecorp.bot.model.profile.UserProfileResponse;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
@@ -41,6 +42,8 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 public class Oripyon_jr {
 	//@Autowired
 	//private final LineMessagingService lineMessagingService;
+	@Autowired
+	private LineMessagingClientImpl lineMessagingClientImpl
 	
 	int seed;
 	Random random = new Random();
