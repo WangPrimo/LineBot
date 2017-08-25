9f525161cdf02d7a72fb5bdc876146fe34ff5f66
王星涵 <primo_wang@yahoo.com.tw>
2017-08-26 03:24:05 +0800

    修改些許文字說明，嘗試增加圖片

--- Oripyon_jr/src/main/java/com/bot/Enum/CommandHelp.java
+++ Oripyon_jr/src/main/java/com/bot/Enum/CommandHelp.java
@@ -3,8 +3,8 @@ package com.bot.Enum;
 public enum CommandHelp {
 	
 	randomArrayCommand("隨機結果指令", "每個指令下有若干個結果，由系統隨機輸出"),
-	binaryCommand("目標指令", "在指令後以一個空白鍵做間隔後再接上目標，使你對目標執行一個動作。若沒有授權機器人取用資料的權限則則由草泥馬代勞"),
-	unaryCommand("動作指令", "發話者自己執行指令的動作，需授權給機器人取用資料的權限才能使用");
+	binaryCommand("目標指令", "在指令後以一個空白鍵做間隔再接上目標，使你對目標執行指令的動作。若沒有授權機器人取用資料的權限則由草泥馬代勞"),
+	unaryCommand("動作指令", "你自己執行指令的動作，需授權給機器人取用資料的權限才能使用");
 	
 	public String chineseCommand;
 	public String description;
